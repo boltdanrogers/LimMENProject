40b263c234d90e1c168fe46830c930e4eb6e4798
Daniel Rogers <boldanrogers@gmail.com>
2023-08-27 22:31:22 -0400

    added about response

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
-    console.log(req.url)
-    res.end('Hello Node.js')
-})
+    if(req.url ==='/about')
+        res.end('The about page')
+    else{
+        res.writeHead(404)
+        res.end('Page Note Found')
+    }//end of else
+   
+   
+    //console.log(req.url)
+   //res.end('Hello Node.js')
+})//end of arrow function
 
 server.listen(3000)
