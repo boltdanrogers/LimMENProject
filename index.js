cf3f4075c1f6bfaed200e80644cc59ef9d2143dd
Daniel John Rogers <69739876+boltdanrogers@users.noreply.github.com>
2023-09-08 21:40:43 -0400

    loaded base project files and installed nodemon

--- index.js
+++ index.js
@@ -1,78 +1,11 @@
-//const http = require('http')
-//require is similar to import of include in other languages
-const express = require('express')//require express module
+const express = require('express')
 
-
-/*const fs = require('fs')
-const homePage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const notFoundPage = fs.readFileSync('notFound.html')
-*/
-
-
-
-const app = express() // calls express function to start new Express app
-
-//in order to get the correct system path to a file, we need:
-const path = require('path')
-
-
+const app = new express()
 app.use(express.static('public'))
-//special function call to tell express where to look for assets
-
-//and now use it to server up a html page
-app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'about.html'))
-})//end of arrow function and get call
-
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'index.html'))
-})//end of arrow function and get call
-
-app.get('/contact',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'contact.html'))
-})//end of arrow function and get call
 
 
-
-//add the express method to define a GET response
-//creating different responses to different urls is called routing
-//unlike the unwieldy if-then clauses used in our single createServer call
-//with express we can refactor into many small handler functions
-app.get('/name',(req,res)=>{
-    res.json({
-        name:'Greg Lim'
-    })//end of json
-
-})//end of arrow function and get function call
+app.listen(4000,()=>{
+    console.log('App is listening on port 4000')
+})
 
 
-/* with the createServer function, the arrow function contained within that call had the logic for different responses in if-else statements
-const server = http.createServer((req,res)=>{
-    console.log(req.url)
-    
-    if(req.url ==='/about')
-        res.end(aboutPage)
-    else if (req.url === '/contact')
-        res.end(contactPage)
-    else if(req.url == '/home')
-        res.end(homePage)
-    else{
-        res.writeHead(404)
-        res.end(notFoundPage)
-    }//end of else
-   
-   //res.end('Hello Node.js')
-})//end of arrow function, call to createServer
-*/
-
-
-
-app.listen(3000, ()=>{
-
-    //add a simple console log to show the app is working
-    console.log("App is listening to port 3000")
-
-})//end of arrow function and listen call
-//server.listen(3000)
